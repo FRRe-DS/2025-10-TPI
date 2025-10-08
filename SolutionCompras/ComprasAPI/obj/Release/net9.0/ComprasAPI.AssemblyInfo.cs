989431455b4486597d9ff70b92b19baec8d4e08c
joaquinsimonek <joaquinsimonek@gmail.com>
2025-10-07 21:25:42 -0300

    prueba git

--- SolutionCompras/ComprasAPI/obj/Release/net9.0/ComprasAPI.AssemblyInfo.cs
+++ SolutionCompras/ComprasAPI/obj/Release/net9.0/ComprasAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComprasAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447b24fd2974a4010673cf5b7347194dd0945318")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComprasAPI")]
